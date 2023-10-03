3c321b42944da0d1a74e7c3eef81144c9c2ab11a
Crowdbotics <team@crowdbotics.com>
2023-10-03 16:23:49 +0000

    Initial commit

---
+++ backend/shrill_rain_43731/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for shrill_rain_43731 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "shrill_rain_43731.settings"
+)
+
+application = get_wsgi_application()
